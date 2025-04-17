3205194dc42a74936c15c22776a87ee58da3eee5
Chloe <cgreenstreet01@gmail.com>
2025-04-17 15:20:01 +0100

    17.04.25 : basic conditional statement for defining screen size

--- lib/layout_widgets/bottom_navigation_bar.dart
+++ lib/layout_widgets/bottom_navigation_bar.dart
@@ -12,8 +12,16 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double iconSize = MediaQuery.of(context).size.width * 0.08;
-    // TODO: Could adjust this for accessibility with setting button
+    double screenWidth = MediaQuery.of(context).size.width;
+    double iconSize;
+
+    if (screenWidth < 600) {
+      // Phone view
+      iconSize = screenWidth * 0.08;
+    } else {
+      // Tablet view
+      iconSize = screenWidth * 0.029;
+    }
 
     return BottomNavigationBar(
       currentIndex: currentIndex,
@@ -22,32 +30,26 @@ class CustomBottomNavigationBar extends StatelessWidget {
       showUnselectedLabels: false,
       items: [
         BottomNavigationBarItem(
-          icon: Flexible(
-            child: SizedBox(
-              width: iconSize,
-              height: iconSize,
-              child: ImageIcon(AssetImage('assets/navigation/reuse.png')),
-            ),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/reuse.png')),
           ),
           label: 'Upcycle',
         ),
         BottomNavigationBarItem(
-          icon: Flexible(
-            child: SizedBox(
-              width: iconSize,
-              height: iconSize,
-              child: ImageIcon(AssetImage('assets/navigation/home.png')),
-            ),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/home.png')),
           ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Flexible(
-            child: SizedBox(
-              width: iconSize,
-              height: iconSize,
-              child: ImageIcon(AssetImage('assets/navigation/guidelines.png')),
-            ),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/guidelines.png')),
           ),
           label: 'Guidelines',
         ),
