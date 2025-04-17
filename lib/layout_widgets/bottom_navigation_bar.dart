e645807a030ae6490748aae9a924ff1358ca1dad
Chloe <cgreenstreet01@gmail.com>
2025-04-17 14:50:29 +0100

    17.04.25 : updated to bolder icons and used dynamic sizing boxing

--- lib/layout_widgets/bottom_navigation_bar.dart
+++ lib/layout_widgets/bottom_navigation_bar.dart
@@ -12,25 +12,36 @@ class CustomBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double iconSize = MediaQuery.of(context)
-        .size.width * 0.08; // Adjust
+    final double iconSize = MediaQuery.of(context).size.width * 0.025; // Adjust
 
     return BottomNavigationBar(
       currentIndex: currentIndex,
       onTap: onTap,
       showSelectedLabels: false,
       showUnselectedLabels: false,
-      items: const [
+      items: [
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage('assets/navigation/reuse.png')),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/reuse.png')),
+          ),
           label: 'Upcycle',
-        ),
+          ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage('assets/navigation/home.png')),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/home.png')),
+          ),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: ImageIcon(AssetImage('assets/navigation/guidelines.png')),
+          icon: SizedBox(
+            width: iconSize,
+            height: iconSize,
+            child: ImageIcon(AssetImage('assets/navigation/guidelines.png')),
+          ),
           label: 'Guidelines',
         ),
       ],
