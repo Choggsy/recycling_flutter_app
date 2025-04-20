2903959a0a9050d99fa26dace461ca7e9c0a86ab
Chloe <cgreenstreet01@gmail.com>
2025-04-20 17:53:45 +0100

    20.04.25 : refactored and extracted tests

--- test/properties/device_view_vector.dart
+++ test/properties/device_view_vector.dart
@@ -2,22 +2,32 @@ import 'package:flutter_test/flutter_test.dart' show closeTo, expect, test;
 import 'package:recycling_flutter_app/properties/device_view_vector.dart' show ScreenConfig;
 
 void main() {
+  final double pointOne = 0.1;
+
   test('ScreenConfig calculates correct icon size for phone screens', () {
-    double smallScreenWidth = 300.0;
-    double largeScreenWidth = 600.0;
-
-    double smallIconSize = ScreenConfig.getIconSize(smallScreenWidth);
-    double largeIconSize = ScreenConfig.getIconSize(largeScreenWidth);
-
-    expect(smallIconSize, closeTo(smallScreenWidth * ScreenConfig.phoneIconSizeFactor, 0.1));
-    expect(largeIconSize, closeTo(largeScreenWidth * ScreenConfig.phoneIconSizeFactor, 0.1));
+    final double largeScreenWidth = 600.0;
+    final double largeIconSize = ScreenConfig.getIconSize(largeScreenWidth);
+    expect(largeIconSize, closeTo(largeScreenWidth * ScreenConfig.phoneIconSizeFactor, pointOne));
   });
 
-  test('ScreenConfig calculates correct icon size for tablet screens', () {
-    double tabletScreenWidth = 800.0;
+  test('ScreenConfig calculates for small phone screens', () {
+    final double smallScreenWidth = 300.0;
+    final double smallIconSize = ScreenConfig.getIconSize(smallScreenWidth);
+    expect(smallIconSize, closeTo(smallScreenWidth * ScreenConfig.phoneIconSizeFactor, pointOne));
+  });
 
-    double tabletIconSize = ScreenConfig.getIconSize(tabletScreenWidth);
 
-    expect(tabletIconSize, closeTo(tabletScreenWidth * ScreenConfig.tabletIconSizeFactor, 0.1));
+  test('ScreenConfig calculates correct icon size for small tablet screens', () {
+    final double tabletScreenWidth = 800.0;
+    final double tabletIconSize = ScreenConfig.getIconSize(tabletScreenWidth);
+
+    expect(tabletIconSize, closeTo(tabletScreenWidth * ScreenConfig.tabletIconSizeFactor, pointOne));
+  });
+
+  test('ScreenConfig calculates correct icon size for large tablet screens', () {
+    final double tabletScreenWidth = 1000.0;
+    final double tabletIconSize = ScreenConfig.getIconSize(tabletScreenWidth);
+
+    expect(tabletIconSize, closeTo(tabletScreenWidth * ScreenConfig.tabletIconSizeFactor, pointOne));
   });
 }
