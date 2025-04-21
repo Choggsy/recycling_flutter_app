5b12bf7cad9d9be82bda5a13ff0693ac58133f04
Chloe <cgreenstreet01@gmail.com>
2025-04-21 09:11:54 +0100

    21.04.25 : tests for top bar. has a title and adjusts to device height

---
+++ test/layout_widgets/top_app_bar_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recycling_flutter_app/layout_widgets/top_app_bar.dart' show CustomAppBar;
+
+void main() {
+  testWidgets('CustomAppBar displays the correct title', (final WidgetTester tester) async {
+    final String appBarTitle = 'Test Title';
+    await appBarWidget(tester, appBarTitle);
+
+    expect(find.text(appBarTitle), findsOneWidget);
+  });
+
+  testWidgets('CustomAppBar has the correct preferred size from device height', (final WidgetTester tester) async {
+    await appBarWidget(tester, 'Test Title');
+    final AppBar appBar = tester.widget(find.byType(AppBar));
+
+    expect(appBar.preferredSize, equals(Size.fromHeight(kToolbarHeight)));
+  });
+}
+
+Future<void> appBarWidget(final WidgetTester tester, final String appBarTitle) async {
+  await tester.pumpWidget(
+    MaterialApp(
+      home: Scaffold(
+        appBar: CustomAppBar(title: appBarTitle),
+      ),
+    ),
+  );
+}
