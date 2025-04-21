5a6e0e62bc85245a6d14d8ef336ea4dc175ff8bd
Chloe <cgreenstreet01@gmail.com>
2025-04-21 23:15:58 +0100

    21.04.25 : adjust view for tablets to be more asthetic for bigger screens

--- lib/component/mosaic_buttons.dart
+++ lib/component/mosaic_buttons.dart
@@ -2,31 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:recycling_flutter_app/component/tile_button.dart' show TileButton;
 import '../helper/grid_helper.dart' show GridHelper;
+import '../properties/device_view_vector.dart' show ScreenConfig, ViewType;
 
 class MosaicButtons {
   static Widget buildMosaicGrid(final BuildContext context, final List labels, final Function getPage) {
-    final Size screenSize = MediaQuery.of(context).size;
-    final bool isTablet = screenSize.width > 600; // Adjust the threshold as needed
+    final ViewType viewType = ScreenConfig.getViewType(MediaQuery.of(context).size.width);
 
     return StaggeredGrid.count(
-      crossAxisCount: isTablet ? 6 : 4, // More columns for tablets
+      crossAxisCount: viewType == ViewType.tablet ? 6 : 4, // More columns for tablets
       mainAxisSpacing: 10.0,
       crossAxisSpacing: 10.0,
-      children: _buildMosaicButtons(labels, getPage, isTablet),
+      children: _buildMosaicButtons(labels, getPage, viewType),
     );
   }
 
-  static List<Widget> _buildMosaicButtons(final List labels, final Function getPage, final bool isTablet) {
+  static List<Widget> _buildMosaicButtons(final List labels, final Function getPage, final ViewType viewType) {
     return labels.asMap().entries.map((entry) {
       final int index = entry.key;
-      final String label = entry.value;
       final int patternIndex = index % 5;
 
       return StaggeredGridTile.count(
         crossAxisCellCount: GridHelper.getCrossAxisCellCount(patternIndex),
-        mainAxisCellCount: isTablet ? GridHelper.getMainAxisCellCount(patternIndex) / 1.5 : GridHelper.getMainAxisCellCount(patternIndex),
+        mainAxisCellCount: viewType == ViewType.tablet ? GridHelper.getMainAxisCellCount(patternIndex) / 1.5
+            : GridHelper.getMainAxisCellCount(patternIndex),
         child: TileButton(
-          label: label,
+          label: entry.value,
           index: index,
           getPage: getPage,
         ),
