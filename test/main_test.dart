e86a85e8d214d7b772a73bfe3b7ff978e33d8347
Chloe <cgreenstreet01@gmail.com>
2025-04-21 22:35:59 +0100

    21.04.25 : all 100% covered except main runner

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,13 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:recycling_flutter_app/main.dart';
 import 'package:recycling_flutter_app/view/home_page.dart';
+import 'package:recycling_flutter_app/properties/app_theme.dart';
 
 void main() {
-
-  testWidgets('MyApp has a title and home page', (WidgetTester tester) async {
+  testWidgets('Verify the home page is displayed', (final WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-
-    // Verify the home page is displayed
     expect(find.byType(HomePage), findsOneWidget);
+  });
+
+  testWidgets('Compare specific properties of the theme', (final WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    final ThemeData theme = Theme.of(tester.element(find.byType(HomePage)));
+
+    expect(theme.primaryColor, equals(AppTheme.lightTheme.primaryColor));
+    expect(theme.colorScheme.surface, equals(AppTheme.lightTheme.colorScheme.surface));
+  });
+
+  testWidgets('Verify the MaterialApp widget is present', (final WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
