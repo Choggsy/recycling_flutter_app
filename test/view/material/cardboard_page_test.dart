50022465e329ba5ab19e0c9ca89db321fe7c83b9
Chloe <cgreenstreet01@gmail.com>
2025-04-21 20:20:07 +0100

    21.04.25 : unit test for cardboard page

---
+++ test/view/material/cardboard_page_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:recycling_flutter_app/view/material/cardboard_page.dart' show CardboardPage;
+
+void main() {
+  testWidgets('Page has a title and body text', (final WidgetTester tester,) async {
+    await tester.pumpWidget(MaterialApp(home: CardboardPage()));
+
+    expect(find.text('Cardboard'), findsOneWidget);
+    expect(find.text('This is the Cardboard page'), findsOneWidget);
+  });
+}
