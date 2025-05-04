22b2dd5193b60f07b65e7e0ff358c8d25487feec
Chloe <cgreenstreet01@gmail.com>
2025-05-04 14:58:17 +0100

    04.05.25 : json parsed

--- lib/view/homepage/home_page.dart
+++ lib/view/homepage/home_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:recycling_flutter_app/component/bottom_navigation_bar.dart' show CustomBottomNavigationBar;
 import 'package:recycling_flutter_app/component/top_app_bar.dart' show CustomAppBar;
@@ -21,85 +23,78 @@ typedef GetPageCallback = Widget Function(int index);
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Future<List<RecyclingFact>> loadRecyclingFacts() async {
+    final String jsonString = await rootBundle.loadString('assets/facts.json');
+    final List<dynamic> jsonData = json.decode(jsonString);
+    return jsonData.map((item) => RecyclingFact.fromJson(item)).toList();
+  }
+
   @override
   Widget build(final BuildContext context) {
-    final List<RecyclingFact> facts = [
-      RecyclingFact(
-        id: 1,
-        title: "Don't recycle anything smaller than a tennis ball",
-        description: "Small items can jam the recycling machinery, causing delays and potential damage.",
-      ),
-      RecyclingFact(
-        id: 2,
-        title: "Rinse out containers before recycling",
-        description: "Food residue can contaminate other recyclables, making them unsuitable for recycling.",
-      ),
-      RecyclingFact(
-        id: 3,
-        title: "Do not recycle plastic bags",
-        description: "Plastic bags can get tangled in the machinery, causing operational issues.",
-      ),
-      RecyclingFact(
-        id: 4,
-        title: "Flatten cardboard boxes",
-        description: "Flattening boxes saves space and makes the recycling process more efficient.",
-      ),
-    ];
-
-
     return Scaffold(
       appBar: CustomAppBar(title: 'Home', showBackButton: false),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            buildDivider(2),
-            Space.medium.box,
-            ButtonStyles.buildTileButton("Map Button", 0, (index) => const MapPage(), context),
-            Space.medium.box,
-            buildDivider(2),
-            Space.small.box,
-            ButtonStyles.buildTileButton("Sorting Guide", 1, (index) => const RecyclableLogoPage(), context),
-            Space.small.box,
-            StaggeredGrid.count(
-              crossAxisCount: 4,
-              mainAxisSpacing: 12,
-              crossAxisSpacing: 12,
-              children: [
-                ButtonStyles.iconTile(
-                  context: context,
-                  index: 2,
-                  getPage: (index) => const BinCollectionPage(),
-                  label: "Bin Collection Days",
-                  assetPath: 'assets/logo/placeholder.jpg',
-                ),
-                ButtonStyles.iconTile(
-                  context: context,
-                  index: 3,
-                  getPage: (index) => const SustainabilityPage(),
-                  label: "Sustainability Page",
-                  assetPath: 'assets/logo/placeholder.jpg',
-                ),
-                ButtonStyles.wideIconTile(
-                  context: context,
-                  index: 4,
-                  getPage: (index) => const InvolvementPage(),
-                  label: "Involvement Page",
-                  assetPath: 'assets/logo/placeholder.jpg',
-                ),
-              ],
-            ),
-            Space.large.box,
-            // Text('Recycling Facts', style: Theme.of(context).textTheme.titleLarge),
-            buildDivider(4),
-            buildRecyclingFactsSection(context, facts),
-            Space.large.box,
-            buildDivider(4),
-            Space.large.box,
-            ButtonStyles.buildTileButton("Recycling News", 5, (index) => const NewsPage(), context)
-          ],
-        ),
+      body: FutureBuilder<List<RecyclingFact>>(
+        future: loadRecyclingFacts(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading facts'));
+          } else {
+            final facts = snapshot.data!;
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  buildDivider(2),
+                  Space.medium.box,
+                  ButtonStyles.buildTileButton("Map Button", 0, (index) => const MapPage(), context),
+                  Space.medium.box,
+                  buildDivider(2),
+                  Space.small.box,
+                  ButtonStyles.buildTileButton("Sorting Guide", 1, (index) => const RecyclableLogoPage(), context),
+                  Space.small.box,
+                  StaggeredGrid.count(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                    children: [
+                      ButtonStyles.iconTile(
+                        context: context,
+                        index: 2,
+                        getPage: (index) => const BinCollectionPage(),
+                        label: "Bin Collection Days",
+                        assetPath: 'assets/logo/placeholder.jpg',
+                      ),
+                      ButtonStyles.iconTile(
+                        context: context,
+                        index: 3,
+                        getPage: (index) => const SustainabilityPage(),
+                        label: "Sustainability Page",
+                        assetPath: 'assets/logo/placeholder.jpg',
+                      ),
+                      ButtonStyles.wideIconTile(
+                        context: context,
+                        index: 4,
+                        getPage: (index) => const InvolvementPage(),
+                        label: "Involvement Page",
+                        assetPath: 'assets/logo/placeholder.jpg',
+                      ),
+                    ],
+                  ),
+                  Space.large.box,
+                  buildDivider(4),
+                  buildRecyclingFactsSection(context, facts),
+                  Space.large.box,
+                  buildDivider(4),
+                  Space.large.box,
+                  ButtonStyles.buildTileButton("Recycling News", 5, (index) => const NewsPage(), context)
+                ],
+              ),
+            );
+          }
+        },
       ),
       bottomNavigationBar: CustomBottomNavigationBar(
         currentIndex: 1,
@@ -129,7 +124,8 @@ class HomePage extends StatelessWidget {
           children: [
             Text(
               'Recycling Facts',
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.black)),
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.black),
+            ),
             TextButton(
               onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (_) => RecyclingFactPage(facts: facts)));
@@ -138,7 +134,7 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        ...facts.take(2).toList().map((fact) => RecyclingFactCard(fact: fact)).toList(),
+        ...facts.take(2).map((fact) => RecyclingFactCard(fact: fact)).toList(),
       ],
     );
   }
