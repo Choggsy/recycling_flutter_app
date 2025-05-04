a0fb1517363bdc6394c571a3fb5474dff4802c5e
Chloe <cgreenstreet01@gmail.com>
2025-05-04 11:53:52 +0100

    04.05.25 : filling out homepage per wireframes (dividers)

--- lib/view/homepage/home_page.dart
+++ lib/view/homepage/home_page.dart
@@ -8,6 +8,7 @@ import 'package:recycling_flutter_app/view/homepage/news_page.dart';
 import 'package:recycling_flutter_app/view/homepage/sustainable_page.dart';
 import '../../component/tile_button.dart';
 import '../../helper/get_page.dart';
+import '../../helper/space_helper.dart';
 import '../../properties/app_theme.dart' show AppColors;
 import '../guideline/logo/recycling_logo_page.dart';
 
@@ -25,10 +26,20 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Divider(
+                thickness: 2,
+                color: AppColors.darkRedBrown
+            ),
+            Space.medium.box,
             buildTileButton("Map Button", 0, (index) => const MapPage()),
-            const SizedBox(height: 40),
+            Space.medium.box,
+            Divider(
+                thickness: 2,
+                color: AppColors.darkRedBrown
+            ),
+            Space.small.box,
             buildTileButton("Sorting Guide", 1, (index) => const RecyclableLogoPage()),
-            const SizedBox(height: 20),
+            Space.small.box,
             StaggeredGrid.count(
               crossAxisCount: 4,
               mainAxisSpacing: 12,
@@ -36,9 +47,21 @@ class HomePage extends StatelessWidget {
               children: [
                 buildIconTileButton(context, 2, (index) => const BinCollectionPage(), "Bin Collection Days", 'assets/logo/placeholder.jpg'),
                 buildIconTileButton(context, 3, (index) => const NewsPage(), "Recycling News", 'assets/logo/placeholder.jpg'),
-                buildIconTileButton(context, 4, (index) => const SustainabilityPage(), "Sustainability Page", 'assets/logo/placeholder.jpg'),
+                buildWideIconTileButton(context, 4, (index) => const SustainabilityPage(), "Sustainability Page", 'assets/logo/placeholder.jpg'),
               ],
             ),
+            Space.large.box,
+            Divider(
+                thickness: 4,
+                color: AppColors.darkRedBrown
+            ),
+            Space.large.box,
+            Divider(
+                thickness: 4,
+                color: AppColors.darkRedBrown
+            ),
+            Space.large.box,
+            buildTileButton("Recycling News", 1, (index) => const NewsPage())
           ],
         ),
       ),
@@ -76,6 +99,45 @@ class HomePage extends StatelessWidget {
     return StaggeredGridTile.count(
       crossAxisCellCount: 2,
       mainAxisCellCount: 2,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => getPage(index)),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(color: AppColors.darkBrown, width: 3.0),
+            color: AppColors.background,
+          ),
+          child: Center(
+            child: Semantics(
+              label: label,
+              child: Image.asset(
+                assetPath,
+                width: 50,
+                height: 50,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildWideIconTileButton(
+      BuildContext context,
+      int index,
+      GetPageCallback getPage,
+      String label,
+      String assetPath,
+      ) {
+    return StaggeredGridTile.count(
+      crossAxisCellCount: 4, // Full width
+      mainAxisCellCount: 2,  // Taller height
       child: GestureDetector(
         onTap: () {
           Navigator.push(
